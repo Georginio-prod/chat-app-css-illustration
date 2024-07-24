580c95f1206fede039d552ed206035b8585b3208
Georginio-prod <etonameklou6@gmail.com>
2024-07-24 04:30:34 +0000

    ajust the position, size for the card compnenté

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ export default {
         body: '#FAFAFA',
         linear1: '#E844FF',
         linear2: '#8739FF',
-        des:'#D99EFF'
+        des:'#D99EFF',
+        font: '#F5F3F7',
+        font2: '#EEE5F4',
+        col:'#9341C8',
+        col1:'#6E5D7E',
+        cercle:'#E472FF',
+        plac:'#C5C9CC',
+        colorT:'#3E2753',
+        colorT2: '#A39DA9'
+
       },
     },
   },
